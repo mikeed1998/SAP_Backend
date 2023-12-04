714a3ac373514e1617d634a66e566245478c3ac3
Michael <49420226+mikeed1998@users.noreply.github.com>
2023-12-04 16:51:10 -0600

    base datos

--- resources/views/configs/secciones/vacantes.blade.php
+++ resources/views/configs/secciones/vacantes.blade.php
@@ -8,7 +8,8 @@
 @endsection
 @section('styleExtras')
 <style>
-        @font-face {
+
+@font-face {
         font-family: 'Neusharp Bold';
         font-style: normal;
         font-weight: normal;
@@ -54,12 +55,12 @@
     }
 
     /* input con opacidad y sin boton de selecciond e archivo */
-                .file-upload input[type="file"], .file-upload2 input[type="file"] {
+                .file-upload input[type="file"] {
                     position: absolute;
                     left: -9999px;
                     }
 
-                    .file-upload label, .file-upload2 label {
+                    .file-upload label {
                     display: inline-block;
                     background-color: #00000031;
                     color: #fff;
@@ -70,7 +71,7 @@
                     opacity: 0%;
                     }
 
-                    .file-upload input[type="file"] + label:before, .file-upload2 input[type="file"] + label:before {
+                    .file-upload input[type="file"] + label:before {
                     content: "\f07b";
                     font-family: "Font Awesome 5 Free";
                     font-size: 16px;
@@ -78,12 +79,12 @@
                     transition: all 0.5s;
                     }
 
-                    .file-upload input[type="file"] + label, .file-upload2 input[type="file"] + label {
+                    .file-upload input[type="file"] + label {
                         transition: all 0.5s;
                     }
 
-                    .file-upload input[type="file"]:focus + label, .file-upload2 input[type="file"]:focus + label,
-                    .file-upload input[type="file"] + label:hover, .file-upload2 input[type="file"] + label:hover {
+                    .file-upload input[type="file"]:focus + label,
+                    .file-upload input[type="file"] + label:hover {
                     backdrop-filter: blur(5px);
                     background-color: #41464a37;
                     opacity: 100%;
@@ -169,79 +170,50 @@
 
        
         <div class="col-12 my-5 text-center d-flex justify-content-center align-items-center flex-column">
-            <p class="mt-5" style="font-size: 4.5rem; font-family:'Neusharp Bold'; color: #909986;">SERVICIOS</p>
+            <p class="mt-5" style="font-size: 4.5rem; font-family:'Neusharp Bold'; color: #909986;">VACANTES</p>
 			<div class="col-6">
-				<div class="col-12 mb-2 text-center"><i class="fa-solid fa-pencil" style="font-size: 1.5rem;"></i></div>
-				<textarea class="col-12 text-center editar_text_seccion_global editarajax" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$elements[0]->id}}" data-table="Elemento" data-campo="texto" name="" id="" cols="30" rows="3" style="border-radius: 10px; border:none; background: #ededed">{{$elements[0]->texto}}</textarea>
+				{{-- <div class="col-12 mb-2 text-center"><i class="fa-solid fa-pencil" style="font-size: 1.5rem;"></i></div>
+				<textarea class="col-12 text-center editar_text_seccion_global editarajax" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$elements[0]->id}}" data-table="elemento" data-campo="texto" name="" id="" cols="30" rows="3" style="border-radius: 10px; border:none; background: #ededed">{{$elements[0]->texto}}</textarea> --}}
 			</div>
         </div>
 
     <div class="col-12">
 
         <div class="col-12 text-center d-flex justify-content-center flex-column">
-            <h4 style="font-family:'Neusharp Bold';">Agregar Servicio</h4>
-            <div>
+            <h4 style="font-family:'Neusharp Bold';">Agregar Vacante</h4>
+            <div >
                 <button style="background: none !important; border:none;" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-circle-plus" style="font-size: 2rem;"></i></button>
             </div>
         </div>
 
         <div class="container d-flex flex-wrap">
-
-            @foreach($servicios as $s)
+            @foreach($vacantes as $v)
             <div class="col-4 p-3 " style="border-radius:26px;">
-                <div class="row">
-                    <div class="col position-relative px-0 card position-relative">
-                        <img src="{{asset('img/photos/servicios/'.$s->portada)}}" alt="" class="img-fluid">
-                        <div class="card d-flex justify-content-center align-items-center w-100 position-absolute bottom-0 start-50 translate-middle bg-transparent border-0">
-							<form id="form_aux-{{ $s->id }}" action="image_input_ejemplo" method="POST" class="file-upload px-auto col-7" enctype="multipart/form-data">
-								@csrf
-								<input type="hidden" name="id_elemento" value="{{ $s->id }}">
-								<input type="hidden" name="tipo" value="solucion_imagen">
-								<input id="img_aux-{{ $s->id }}" class="m-0 p-0" type="file" name="archivo">
-								<label class="col-12 m-0 px-2 d-flex justify-content-center align-items-center" for="img_aux-{{ $s->id }}" style=" height: 100%; opacity: 100%; border-radius: 20px;">Actualizar Portada</label>
-							</form>
-							<script>
-								$('#img_aux-{{ $s->id }}').change(function(e) {
-									$('#form_aux-{{ $s->id }}').trigger('submit');
-								});
-							</script>
-						</div>
-                        <div class="card d-flex justify-content-center align-items-center w-100 position-absolute top-0 start-0 bg-transparent border-0">
-							<form id="iform_aux-{{ $s->id }}" action="image_input_ejemplo" method="POST" class="file-upload px-auto col-7" enctype="multipart/form-data">
-								@csrf
-								<input type="hidden" name="id_elemento" value="{{ $s->id }}">
-								<input type="hidden" name="tipo" value="solucion_icono">
-								<input id="iimg_aux-{{ $s->id }}" class="m-0 p-0" type="file" name="archivo">
-								<label class="col-12 m-0 px-2 d-flex justify-content-center align-items-center" for="iimg_aux-{{ $s->id }}" style=" height: 100%; opacity: 100%; border-radius: 20px;">Actualizar Icono</label>
-							</form>
-							<script>
-								$('#iimg_aux-{{ $s->id }}').change(function(e) {
-									$('#iform_aux-{{ $s->id }}').trigger('submit');
-								});
-							</script>
-						</div>
-                        <div class="col-3 card position-absolute">
-                            <div class="row">
-                                <img src="{{asset('img/photos/servicios/'.$s->imagen)}}" alt="" class="img-fluid">
-                            </div>
-                            <div class="row">
-                                <div class="col text-center p-3">
-                                    <input type="number" class="form-control fw-bold editar_text_seccion_global" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$s->id}}" data-table="ZServicio" data-campo="orden" value="{{$s->orden}}" style="box-shadow: 0;" placeholder="Orden">
-                                </div>
-                            </div>
-                        </div>
+                <div class="card position-relative">
+                    <img src="{{asset('img/photos/vacantes/'.$v->portada)}}" alt="" class="img-fluid">
+                    <div class="card w-100 position-absolute top-50 start-50 translate-middle bg-transparent border-0">
+                        <form id="form_aux-{{ $v->id }}" action="image_input_ejemplo" method="POST" class="file-upload px-auto col-7" enctype="multipart/form-data">
+                            @csrf
+                            <input type="hidden" name="id_elemento" value="{{ $v->id }}">
+                            <input type="hidden" name="tipo" value="vacante">
+                            <input id="img_aux-{{ $v->id }}" class="m-0 p-0" type="file" name="archivo">
+                            <label class="col-12 m-0 px-2 d-flex justify-content-center align-items-center" for="img_aux-{{ $v->id }}" style=" height: 100%; opacity: 100%; border-radius: 20px;">Actualizar Imagen</label>
+                        </form>
+                        <script>
+                            $('#img_aux-{{ $v->id }}').change(function(e) {
+                                $('#form_aux-{{ $v->id }}').trigger('submit');
+                            });
+                        </script>
                     </div>
                 </div>
-                
                 <div class="col-12 mt-3 d-flex flex-column" style="text-align: justify;">
-                    <h5><input type="text" class="form-control editar_text_seccion_global" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$s->id}}" data-table="ZServicio" data-campo="titulo" value="{{$s->titulo}}"></h5>
-                    <textarea name="" id="" cols="30" rows="3" class="form-control" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$s->id}}" data-table="ZServicio" data-campo="descripcion">{{$s->descripcion}}</textarea>
-                    <textarea name="" id="" cols="30" rows="3" class="form-control" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$s->id}}" data-table="ZServicio" data-campo="descripcion2">{{$s->descripcion2}}</textarea>
+                    
+                    <h5><input type="text" class="form-control editar_text_seccion_global" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$v->id}}" data-table="ZVacante" data-campo="titulo" value="{{$v->titulo}}"></h5>
                     {{-- <hr class="my-2"> --}}
-					{{-- <textarea class="col-12 mx-0 scrollux editar_text_seccion_global" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$s->id}}" data-table="services" data-campo="descripcion"  cols="30" rows="5" style="background: #f2f2f2; border:none; border-radius: 10px;">{{$s->descripcion}}</textarea> --}}
+					<textarea class="col-12 mx-0 scrollux editar_text_seccion_global" data-url="{{route('config.seccion.textglobalseccion')}}" data-id="{{$v->id}}" data-table="ZVacante" data-campo="descripcion"  cols="30" rows="5" style="background: #f2f2f2; border:none; border-radius: 10px;">{{$v->descripcion}}</textarea>
                     <form action="{{route('config.seccion.elimProy')}}" method="POST" class="col-12 text-center">
                         @csrf
-                        <input type="text" name="id_proy" value="{{$s->id}}" hidden>
+                        <input type="text" name="id_proy" value="{{$v->id}}" hidden>
                         <style>
                             .fa-trash:hover{
                                 opacity: 50%;
@@ -265,13 +237,13 @@
         </div>
     </div> --}}
 
-{{-- modal agregar proyecto --}}
-<form action="{{route('config.seccion.agservicio')}}" method="POST" class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" enctype="multipart/form-data">
+{{-- modal agregar vacante --}}
+<form action="{{route('config.seccion.agproyect')}}" method="POST" class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" enctype="multipart/form-data">
     <div class="modal-dialog modal-dialog-centered">
     <div  class="modal-content" style="border-radius: 16px;" >
         @csrf
         <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Agregar servicio</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Agregar vacante</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -281,34 +253,19 @@
                 }
             </style>
             <div class="col-12 mb-2" style="height: 100px; position: relative;">
-                <div  class="file-upload col-12 p-0 m-0" style=" top: 0; bottom: 0; background: ; height: 100%;" >
-                    @csrf
-                    <input id="input_img_element" class="m-0 p-0" type="file" name="archivo">
-                    <label id="label_form" class="col-12 m-0 p-0 d-flex justify-content-center align-items-center" for="input_img_element" style="opacity: 100%; height: 100%;  border-radius: 16px;">Agregar icono</label>
-                </div>
-                <script>
-                    ///////////////////// Editar campos imegn categoria ////////////////////
-                    $('#input_img_element').change(function(e) {
-                        $('#label_form').addClass('backr');
-                        $('#label_form').html('Imagen añadida');
-                    });
-                    ///////////////////// Editar campos imegn categoria ////////////////////
-                </script>
-            </div>
-            <div class="col-12 mb-2" style="height: 100px; position: relative;">
-                <div  class="file-upload2 col-12 p-0 m-0" style=" top: 0; bottom: 0; background: ; height: 100%;" >
-                    @csrf
-                    <input id="input_img_element2" class="m-0 p-0" type="file" name="archivo2">
-                    <label id="label_form2" class="col-12 m-0 p-0 d-flex justify-content-center align-items-center" for="input_img_element2" style="opacity: 100%; height: 100%;  border-radius: 16px;">Agregar imagen</label>
-                </div>
-                <script>
-                    ///////////////////// Editar campos imegn categoria ////////////////////
-                    $('#input_img_element2').change(function(e) {
-                        $('#label_form2').addClass('backr2');
-                        $('#label_form2').html('Imagen añadida');
-                    });
-                    ///////////////////// Editar campos imegn categoria ////////////////////
-                </script>
+                    <div  class="file-upload col-12 p-0 m-0" style=" top: 0; bottom: 0; background: ; height: 100%;" >
+                        @csrf
+                        <input id="input_img_element" class="m-0 p-0" type="file" name="archivo">
+                        <label id="label_form" class="col-12 m-0 p-0 d-flex justify-content-center align-items-center" for="input_img_element" style="opacity: 100%; height: 100%;  border-radius: 16px;">Seleccionar archivo</label>
+                    </div>
+                    <script>
+                        ///////////////////// Editar campos imegn categoria ////////////////////
+                        $('#input_img_element').change(function(e) {
+                            $('#label_form').addClass('backr');
+                            $('#label_form').html('Imagen añadida');
+                        });
+                        ///////////////////// Editar campos imegn categoria ////////////////////
+                    </script>
             </div>
             <div class="col-12 mb-2">
                 <label for="colorPicker" class="form-label">Selecciona un color de fondo:</label>
@@ -318,17 +275,11 @@
                 <input type="number" name="orden" class="form-control" placeholder="Orden">
             </div>
             <div class="col-12 mb-2">
-                <input class="form-control" type="text" name="nom_sol" placeholder="Nombre del servicio">
+                <input class="form-control" type="text" name="titulov" placeholder="Nombre de la vacante">
             </div>
-            <div class="col-12 mb-2">
-                <textarea class="form-control" type="text" rows="6" name="desc_der" placeholder="Descripción der del servicio"></textarea>
+            <div class="col-12">
+                <textarea class="col-12 form-control" name="descripcionv" id="" rows="5" placeholder="Descripción de la vacante"></textarea>
             </div>
-            <div class="col-12 mb-2">
-                <textarea class="form-control" type="text" rows="6" name="desc_izq" placeholder="Descripción izq del servicio"></textarea>
-            </div>
-            {{-- <div class="col-12">
-                <textarea class="col-12 form-control" name="desc_proy" id=""  rows="5"></textarea>
-            </div> --}}
         </div>
         <div class="modal-footer">
         <div class="btn btn-secondary" data-bs-dismiss="modal" style="background: red !important; border:none;">Cerrar</div>
@@ -337,7 +288,7 @@
     </div>
     </div>
 </form>
-{{-- modal agregar proyecto --}}
+{{-- modal agregar vacante --}}
 @endsection
 @section('jsLibExtras2')
 <script src="{{asset('js/dropify.js')}}" charset="utf-8"></script>
